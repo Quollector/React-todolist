81b1262f7f2011e4594d96ac51f9b4408b9ddc1d
Quentin <quentin.lequenne@gmail.com>
2023-01-02 14:37:10 -0500

    Add components

---
+++ src/components/Item.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default function Item(){
+    return(
+        <li className="rounded-md bg-gradient-to-r from-purple-400 to-puple-200 box-border py-4 px-6 flex justify-between content-center gap-3 flex-wrap">
+            <p className="text-white flex items-center">Coder avec react</p>
+            <button className="bg-pink-400 text-white text-m hover:bg-pink-600 rounded w-max px-3 py-1 transition transition-300">Supprimer</button>
+        </li>
+    );
+}
